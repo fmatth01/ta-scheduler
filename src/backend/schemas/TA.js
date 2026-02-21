7df3c87031f207159cef577ec6438b6aab8bd520
ermiayn3962 <113037878+ermiayn3962@users.noreply.github.com>
2026-02-21 16:53:57 -0500

    Initial Schemas

---
+++ src/backend/schemas/TA.js
@@ -0,0 +1,19 @@
+const Joi = required('joi');
+
+const ta_schema = Joi.object({
+    ta_id: Joi.string().required(),
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    is_tf: Joi.boolean().required(),
+    lab_perm: Joi.number().min(0).max(2), /* 0 - OH, 1 - LabAssist, 2 - LabLead */
+    preferences: Joi.array().items(preference_schema).default([]),
+    confirmed_shifts: Joi.array().items(Joi.number()).default([])
+})
+
+const preference_schema = Joi.object({
+    shift_id: Joi.number(),
+    preference: Joi.number().min(0).max(2), /* 0 - Unavailable, 1 - Available, 2 - Preferred */
+})
+
+
+module.exports = { ta_schema }
---
+++ src/backend/schemas/schedule.js
@@ -0,0 +1,29 @@
+const Joi = required('joi');
+
+const shifts_schema = Joi.object({
+    shift_id: Joi.number().required(),
+    start_time: Joi.date().required(),
+    end_time: Joi.date().required(),
+    is_lab: Joi.boolean().required(),
+    is_empty: Joi.boolean().default(true),
+    tas_scheduled: Joi.array.items(Joi.string()).default([]),
+    staffing_capacity: staffing_capacity_schema
+})
+
+const staffing_capacity_schema = Joi.object({
+    num_tas: Joi.number().min(1).required(),
+    lab_perm: Joi.number().min(0).max(2).required(), /* 0 - OH, 1 - LabAssist, 2 - LabLead */
+})
+
+const schedule_schema = Joi.object({
+    schedule_id: Joi.number().required(),
+    monday: Joi.array(Joi.number()),
+    tuesday: Joi.array(Joi.number()),
+    wednesday: Joi.array(Joi.number()),
+    thursday: Joi.array(Joi.number()),
+    friday: Joi.array(Joi.number()),
+    saturday: Joi.array(Joi.number()),
+    sunday: Joi.array(Joi.number()),
+})
+
+module.exports = { shifts_schema, schedule_schema }
