b091a250db7b668b7d5e819d33debdb59e0cf883
ermiayn3962 <113037878+ermiayn3962@users.noreply.github.com>
2026-02-21 20:14:36 -0500

    Updated server.js to support vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,13 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+}
+
+module.exports = app;
